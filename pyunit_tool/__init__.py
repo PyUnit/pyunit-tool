dfd73166ed07c0a3dec022ff12ec3489ac400cfd
jtyoui <jtyoui@qq.com>
2020-03-10 14:21:54 +0800

    更新版本

--- pyunit_tool/__init__.py
+++ pyunit_tool/__init__.py
@@ -5,7 +5,7 @@
 # @Author: Jtyoui@qq.com
 from .tool import *
 
-__version__ = '2020.2.21'
+__version__ = '2020.3.10'
 __author__ = 'Jtyoui'
 __description__ = 'Python常用工具类。C语言编写'
 __email__ = 'jtyoui@qq.com'
